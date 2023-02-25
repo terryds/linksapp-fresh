0168cdbbb090d9b4da380d80fff284abe5a0e639
user <terrydjony@gmail.com>
2023-02-26 00:19:20 +0700

    updated

--- components/ReadmeButtonComponent.tsx
+++ components/ReadmeButtonComponent.tsx
@@ -4,7 +4,7 @@ export default function ReadmeButtonComponent() {
       class="mb-4 font-mono bg-gray-100 hover:bg-gray-200 text-gray-700 font-bold text-sm py-1 px-4 rounded-lg cursor-pointer mt-2"
       href="/readme"
     >
-      README.md
+      About me
     </a>
   );
 }
